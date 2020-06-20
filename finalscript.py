316031cee085dafa3246c7d2efd371559da7d361
makerdiaryid <66455932+makerdiaryid@users.noreply.github.com>
2020-06-20 13:36:30 +0800

    Add files via upload

--- finalscript.py
+++ finalscript.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
-# The line above tells the Raspberry Pi that we want to run this
-#   script in Python 3 - so we don't have to tell it later
 
 import board
 import neopixel
 from time import sleep
 
-# An example program that cycles through Red, Green Blue and then stops
-
 LEDcount = 8
 
 pixels = neopixel.NeoPixel(board.D18, LEDcount)
 
-#red
-pixels.fill((100,0,0))
-sleep(1)
-#green
-pixels.fill((0,100,0))
-sleep(1)
-#blue
-pixels.fill((0,0,100))
-sleep(1)
+while True:
+  pixels.fill((0,0,150))
+  sleep(1)
+  pixels.fill((0,0,130))
+  sleep(1)
+  pixels.fill((0,0110))
+  sleep(1)
+  pixels.fill((0,0,90))
+  sleep(1)
+  pixels.fill((0,0,70))
+  sleep(1)
+  pixels.fill((0,0,50))
+  sleep(1)
+  pixels.fill((0,0,70))
+  sleep(1)
+  pixels.fill((0,0,90))
+  sleep(1)
+  pixels.fill((0,0,110))
+  sleep(1)
+  pixels.fill((0,0,130))
+  sleep(1)
 
-# and off
-pixels.fill((0,0,0))
